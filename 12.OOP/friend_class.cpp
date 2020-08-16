885198699cd1ba28ade3f2fff7d078d3dc0980f1
chy0428 <21800758@handong.edu>
2020-08-16 23:21:21 +0900

    modify

--- 12.OOP/friend_class.cpp
+++ 12.OOP/friend_class.cpp
@@ -1,37 +1,40 @@
 #include <iostream>
+using namespace std;
 
-using namespace std ;
+class buyer {
+    private:
+        int haveMoney;
 
-class buyer{
-private:
-    int haveMoney;
-public:
-    buyer(int money){
-        haveMoney = money;
-    }
-    void showMoney() {
-        cout << "Buyer Money: " << haveMoney << endl;
-    }
-    friend class seller ; // friend class
+    public:
+        buyer(int money) {
+            haveMoney = money;
+        }
+        void showMoney() {
+            cout << "Buyer Money: " << haveMoney << endl;
+        }
+
+        friend class seller; // friend class
 };
 
 class seller {
-private:
-    int totalMoney;
-public:
-    seller(int money){
-        totalMoney = money;
-    }
-    void showMoney() {
-        cout << "Seller Money: " << totalMoney << endl;
-    }
-    void sellProduct(buyer& b , int price){ //
-        this->totalMoney += price;
-        b.haveMoney -= price;
-    }
+    private:
+        int totalMoney;
+
+    public:
+        seller(int money) {
+            totalMoney = money;
+        }
+        void showMoney() {
+            cout << "Seller Money: " << totalMoney << endl;
+        }
+        void sellProduct(buyer& b , int price) {
+            this->totalMoney += price;
+            b.haveMoney -= price;
+        }
 };
-int main(void){
-    seller *SELLER = new seller(100000) ;
+
+int main()  {
+    seller *SELLER = new seller(100000);
     buyer *BUYER = new buyer(30000);
     
     SELLER->showMoney();
